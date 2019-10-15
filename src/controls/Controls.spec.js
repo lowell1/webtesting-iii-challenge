f0c9c34137afba60dfeeffc2c85ec0ba944c99d0
lowell1 <lowelljacobs@mail.com>
2019-10-14 19:53:08 -0700

    gate tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from "react";
+import Controls from "./Controls";
+import {render, fireEvent, act} from "@testing-library/react";
+
+describe("Controls", () => {
+    it("defaults to unlocked and open", () => {
+        const {getByText} = render(<Controls/>);
+        getByText(/Lock Gate/i);
+        getByText(/Close Gate/i);
+    })
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from "react";
+import Dashboard from "./Dashboard";
+import renderer from "react-test-renderer";
+
+describe("Dashboard", () => {
+    it("shows the controls and display", () => {
+        const tree = renderer.create(<Dashboard/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
